26d2557d7fb6eeb8a05ac014d84c66ba4547dab1
ChifaKing <103551391+ChifaKing@users.noreply.github.com>
2022-06-22 12:37:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ jstoggle.addEventListener("click", () => {
   const audio = document.getElementById("bgm");
 
   if (detectMob()) {
-    audio.volume = 0.05;
+    audio.volume = 0.03;
   } else {
     audio.volume = 0.2;
   }
