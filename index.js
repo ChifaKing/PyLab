05fbefd8f0d9237ce4f07a948b7d23cda7de526f
ChifaKing <103551391+ChifaKing@users.noreply.github.com>
2022-06-22 02:50:17 -0700

    1.1.1 mobile draggable support

--- index.js
+++ index.js
@@ -71,3 +71,31 @@ $("#output").on("DOMSubtreeModified", function () {
     })
     .remove();
 });
+
+window.onload = function () {
+  // find the element that you want to drag.
+  var box = document.getElementById("player");
+
+  /* listen to the touchMove event,
+  every time it fires, grab the location
+  of touch and assign it to box */
+
+  box.addEventListener("touchmove", function (e) {
+    // grab the location of touch
+    var touchLocation = e.targetTouches[0];
+
+    // assign box new coordinates based on the touch.
+    box.style.left = touchLocation.pageX - 63 + "px";
+    box.style.top = touchLocation.pageY - 63 + "px";
+  });
+
+  /* record the position of the touch
+  when released using touchend event.
+  This will be the drop position. */
+
+  box.addEventListener("touchend", function (e) {
+    // current box position.
+    var x = parseInt(box.style.left);
+    var y = parseInt(box.style.top);
+  });
+};
